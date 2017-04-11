1651ad70156056b155213109638f14ffe3ad0735
Alfie "Azelphur" Day <support@azelphur.com>
2017-04-11 21:14:00 +0100

    Update URL in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='qube',
       version='0.1',
       description='A Library to access Qube Smartbulbs API',
-      url='https://github.com/Azelphur/pyPushBullet',
+      url='https://github.com/Azelphur/qube-api',
       author='Azelphur',
       author_email='support@azelphur.com',
       license='GPL',
